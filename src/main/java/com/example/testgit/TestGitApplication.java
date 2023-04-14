d0a1886646c2a39d892db410f36b910018ecce64
yipuxilong <1950769165@qq.com>
2023-04-15 00:18:34 +0800

    modify file

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -8,6 +8,7 @@ public class TestGitApplication {
 
     public static void main(String[] args) {
         System.out.println("test github");
+        System.out.println("study base koowledge");
         SpringApplication.run(TestGitApplication.class, args);
     }
 
